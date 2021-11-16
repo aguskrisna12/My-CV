ab0c86f1f4067abbf36c7715fa22b20236fb643c
aguskrisna12 <aguskrisnau317@gmail.com>
2021-11-17 00:13:09 +0800

    commit1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 // import logo from './logo.svg';
 import './App.css';
+import './image';
+import img from './Images/Profile.jpg';
+
 
 function App() {
   return (
     <div className="App">
       <div>
         <h2>My Portos</h2>
+        <div className="div1">
+          <img className="profile" src={img} alt=""/>
+          <span className="hello">Hello</span>
+          <span>A Bit About Me</span>
+        </div>
+        <div className="div2">
+          <span className="circle1"></span>
+          <span className="circle2"></span>
+          <span className="circle3"></span>
+        </div>
       </div>
     </div>
   );
---
+++ src/image.js
@@ -0,0 +1,15 @@
+import React from "react";
+import img1 from './Images/Profile.jpg'
+
+
+function Images(){
+
+    return(
+
+        <div>
+            <img src={img1} alt="Broke Photo"/>
+        </div>
+    )
+}
+
+export default Images;
