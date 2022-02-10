551350188e6c10ac761f51d1d26606196af535bb
aguskrisna12 <aguskrisnau317@gmail.com>
2022-02-10 10:35:20 +0800

    10Feb

--- src/Pages/Project.js
+++ src/Pages/Project.js
@@ -17,11 +17,6 @@ function Project () {
         imgUrl: img2,
         link : <Link className="btn btn-success" to="./todolist">Here</Link>
         },
-        {   title: "TodoList", 
-        description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.",
-        imgUrl: img2,
-        link : <Link className="btn btn-success" to="./todolist">Here</Link>
-        },
         {   title: "The Memory Game", 
         description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.",
         imgUrl: img3,
